0a867af2f3465087361fec641582250067c31cf0
Den <denisanatoliyovich@gmail.com>
2020-04-07 13:34:19 +0300

    Updates

---
+++ precache-manifest.c8c8e4a03ee6f8e557db77ac3d4f74f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111fca66d40808e0bb57a32d81069dd7",
+    "url": "/weather-app-react-js/index.html"
+  },
+  {
+    "revision": "ae841508c6a18b25a466",
+    "url": "/weather-app-react-js/static/css/main.e8001397.chunk.css"
+  },
+  {
+    "revision": "68af229da33ac894e3dc",
+    "url": "/weather-app-react-js/static/js/2.62fb1450.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/weather-app-react-js/static/js/2.62fb1450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae841508c6a18b25a466",
+    "url": "/weather-app-react-js/static/js/main.a65e326c.chunk.js"
+  },
+  {
+    "revision": "2d47c457c793328e0836",
+    "url": "/weather-app-react-js/static/js/runtime-main.16ef44c3.js"
+  }
+]);
